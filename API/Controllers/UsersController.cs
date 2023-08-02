2d29d5b4d312b2b91f59169a1f407470e2676f5c
Dzavialov <dimazua1409@gmail.com>
2023-08-02 16:19:29 +0300

    Init commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() => await _context.Users.ToListAsync();
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id) => await _context.Users.FindAsync(id);
+    }
+}
